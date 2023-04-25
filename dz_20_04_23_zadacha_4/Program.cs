b40739c7cab39e609783fc1159bc2ddfb25a7546
sajinandrey <sajinandrey@example.com>
2023-04-25 19:41:59 +0300

    выполнил 2е задание

---
+++ dz_20_04_23_zadacha_4/Program.cs
@@ -0,0 +1,28 @@
+﻿//Задача 4: Напишите программу, которая принимает на вход три числа и выдаёт максимальное из этих чисел.
+
+//2, 3, 7 -> 7
+//44 5 78 -> 78
+//22 3 9 -> 22
+
+Console.Write ("Введите превое число: ");
+int a = Convert.ToInt32(Console.ReadLine());
+Console.Write ("Введите второе число: ");
+int b = Convert.ToInt32(Console.ReadLine());
+Console.Write ("Введите третье число: ");
+int c = Convert.ToInt32(Console.ReadLine());
+int max;
+
+
+
+if (a > b && a > c ) max = a;
+    
+    else if (b > a && b > c) max = b; 
+
+        else max = c;          
+                
+Console.Write ("И так Александр!!! бар-ра-бан-на-я дробь и ЭТО: ");
+Console.WriteLine(max);
+         
+        
+
+
