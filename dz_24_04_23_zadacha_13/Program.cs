5e1c5c7e362034cf2af29996aa24b57a9dde3810
sajinandrey <sajinandrey@example.com>
2023-04-30 17:02:25 +0300

    ДЗ задача 13

---
+++ dz_24_04_23_zadacha_13/Program.cs
@@ -0,0 +1,27 @@
+﻿//Напишите программу, которая выводит третью цифру заданного числа или сообщает, что третьей цифры нет.
+//645 -> 5
+//78 -> третьей цифры нет
+//32679 -> 6
+
+Console.Write ("Введите число: ");
+
+
+int number = Convert.ToInt32(Console.ReadLine());
+
+if (number > 100)
+
+{
+int a = number / 1000;
+
+int b = a * 1000;
+
+int c = number - b;
+
+int d = c / 100;
+
+Console.Write ("Второе число: ");
+Console.WriteLine (d);
+}
+else
+
+ Console.Write ("Третей цыфры нету");  
