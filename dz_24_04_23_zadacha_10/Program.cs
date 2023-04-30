7a265f58d5a86fed48573150935f4344d3086443
sajinandrey <sajinandrey@example.com>
2023-04-30 16:54:38 +0300

    ДЗ задача 10

---
+++ dz_24_04_23_zadacha_10/Program.cs
@@ -0,0 +1,27 @@
+﻿//Напишите программу, которая принимает на вход трёхзначное число и на выходе показывает вторую цифру этого числа.
+//456 -> 5
+//782 -> 8
+//918 -> 1
+
+Console.Write ("Введите трехзначное число: ");
+
+
+int number = Convert.ToInt32(Console.ReadLine());
+
+if (number > 100 && number < 1000)
+
+{
+int a = number / 100;
+
+int b = a * 100;
+
+int c = number - b;
+
+int d = c / 10;
+
+Console.Write ("Второе число: ");
+Console.WriteLine (d);
+}
+else
+
+ Console.Write ("Вы ввели не трехзначное число");    
