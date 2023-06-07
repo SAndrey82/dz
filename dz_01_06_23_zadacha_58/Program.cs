8684fda31475dc497a924e3fe0da550a47fd021e
sajinandrey <sajinandrey@example.com>
2023-06-07 12:47:42 +0300

    дз задание 58

---
+++ dz_01_06_23_zadacha_58/Program.cs
@@ -0,0 +1,48 @@
+﻿//Задача 58: Задайте две матрицы. Напишите программу, которая будет находить произведение двух матриц.
+//Например, даны 2 матрицы:
+//2 4 | 3 4
+//3 2 | 3 3
+//Результирующая матрица будет:
+//18 20
+//15 18
+
+int[,] matrix1 = {
+            {2, 4},
+            {3, 2}
+        };
+
+        int[,] matrix2 = {
+            {3, 4},
+            {3, 3}
+        };
+
+        if (matrix1.GetLength(1) != matrix2.GetLength(0))
+        {
+            Console.WriteLine("Невозможно выполнить умножение матриц");
+            return;
+        }
+
+        int rows = matrix1.GetLength(0);
+        int columns = matrix2.GetLength(1);
+        int[,] resultMatrix = new int[rows, columns];
+
+        for (int i = 0; i < rows; i++)
+        {
+            for (int j = 0; j < columns; j++)
+            {
+                for (int k = 0; k < matrix1.GetLength(1); k++)
+                {
+                    resultMatrix[i, j] += matrix1[i, k] * matrix2[k, j];
+                }
+            }
+        }
+
+        Console.WriteLine("Результирующая матрица:");
+        for (int i = 0; i < rows; i++)
+        {
+            for (int j = 0; j < columns; j++)
+            {
+                Console.Write(resultMatrix[i, j] + " ");
+            }
+            Console.WriteLine();
+        }
